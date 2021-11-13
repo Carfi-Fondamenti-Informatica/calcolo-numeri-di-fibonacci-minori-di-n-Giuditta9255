2b430e8d1e923ba57b17512435886cef53b48baa
Giuditta9255 <91844686+Giuditta9255@users.noreply.github.com>
2021-11-13 13:02:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
+   int suc=1, prec=0, ris=0, n=0, i=0;
+   cin>>n;
+   prec=i;
+   if(n!=0){
+      cout<<"1"<<endl;
+   }
+   for(int i=1; i<=n; i++){
+      ris=(prec+suc);
+      prec=suc;
+      suc=ris;
+      if(ris<=n){
+         cout<<ris<<endl;
+      }
+   }
    return 0;
 }
